346a7d9f33486d184b428ec90ea2300954eda8a4
tenshi-mori <tenshi.ciaco.mori@gmail.com>
2023-11-19 01:45:13 -0800

    Added blendSpace2D to sync with cursor-player angle.

--- Characters/player_cat.cs
+++ Characters/player_cat.cs
@@ -5,7 +5,21 @@ public partial class player_cat : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set; } = 200;
-	public void GetInput()
+    private float orientation { get; set; } = 0.0f;
+
+    private AnimationTree _animationTree;
+
+    public override void _Ready()
+    {
+		_animationTree = GetNode<AnimationTree>("AnimationTree");
+    }
+    public void SetSpriteOrientation()
+    {
+        Vector2 direction = (GetGlobalMousePosition() - GlobalPosition).Normalized();
+		direction.Y = direction.Y * (-1);
+		_animationTree.Set("parameters/blend_position", direction);
+    }
+    public void GetInput()
 	{
 		Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
 		if (!inputDirection.IsNormalized())
@@ -14,9 +28,18 @@ public partial class player_cat : CharacterBody2D
 		}
 		Velocity = inputDirection;
 	}
+	public void GetRotation()
+	{
+		Vector2 mousePosition = GetGlobalMousePosition();
+		Vector2 direction = (mousePosition - GlobalPosition).Normalized();
+
+		Rotation = Mathf.Atan2(direction.Y, direction.X);
+
+	}
 	public override void _PhysicsProcess(double delta)
 	{
 		GetInput();
+		SetSpriteOrientation();
 		Velocity = Velocity * Speed;
 		MoveAndSlide();
     }
